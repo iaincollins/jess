3aa92c3d42eb63ad40e56c094b20bca6f3025379
Iain Collins <me@iaincollins.com>
2014-04-09 01:14:40 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ jess
 ====
 
 Jess converts Postman API collections to JavaScript libraries
+
+http://jess.iaincollins.com
---
+++ server.js
@@ -0,0 +1,54 @@
+/**
+ * Jess - Converts Postman API collections to JavaScript libraries
+ * @author      me@iaincollins.com
+ */
+
+var express = require('express');
+var partials = require('express-partials');
+var ejs = require('ejs');
+var Q = require('q');
+var jess = require(__dirname + '/lib/jess');
+
+// Initialise and configure Express and Express Partials
+var app = express();
+app.use(express.static(__dirname + '/public'))
+app.use(partials());
+app.set('title', 'Jess - Converts Postman API collections to JavaScript libraries');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('ejs', ejs.__express);
+partials.register('.ejs', ejs);
+
+app.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+app.get('/collections/', function(req, res, next) {
+    res.redirect('/');
+});
+
+app.get('/collections/:collection', function(req, res, next) {
+    // Ignore optional file extentions (e.g. ".js")
+    var path = req.params.collection.split('.');
+    var collectionId = path[0].replace(/_/g, ' ');
+    var json = collectionId;
+    
+    jess.getCollection(collectionId)
+    .then(function(collection) {
+         return jess.collectionToJavaScript(collection);
+    })
+    .then(function(javascript) {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.send(javascript);
+    });
+});
+
+/**
+ * Handle all other requests as 404 / Page Not Found errors
+ */
+app.use(function(req, res, next) {
+    res.status(404).render('page-not-found', { title: "Page not found" });
+});
+
+app.listen(3030);
